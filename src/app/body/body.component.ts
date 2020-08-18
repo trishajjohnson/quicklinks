096961435c486db3fd3d1d3fb0504c38a0bfdf0c
contactomarnow@gmail.com <contactomarnow@gmail.com>
2020-08-18 17:49:10 -0500

    ui-updates

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,6 +7,121 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+  paths = [
+    {
+      name: 'Dashboard**',
+      path: "",
+    },
+    {
+      name: 'Orders',
+      path: "",
+    },
+    {
+      name: 'Orders (Add)',
+      path: "",
+    },
+    {
+      name: 'Products',
+      path: "",
+    },
+    {
+      name: 'Products (Add)',
+      path: "",
+    },
+    {
+      name: 'Products (Options)',
+      path: "",
+    },
+    {
+      name: 'Products (Filtering)',
+      path: "",
+    },
+    {
+      name: 'Products (Brands)',
+      path: "",
+    },
+    {
+      name: 'Products (Reviews)',
+      path: "",
+    },
+    {
+      name: 'Product Categories',
+      path: "",
+    },
+    {
+      name: 'Customers',
+      path: "",
+    },
+    {
+      name: 'Customers (Add)',
+      path: "",
+    },
+    {
+      name: 'Customers (Groups)',
+      path: "",
+    },
+    {
+      name: 'Storefront (My Themes)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Theme Marketplace)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Logo)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Carousel)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Social)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Footer Scripts)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Web Pages)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Blog)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Image Manager)',
+      path: "",
+    },
+    {
+      name: 'Storefront (Email Templates)',
+      path: "",
+    },
+    {
+      name: 'Marketing (Banners)',
+      path: "",
+    },
+    {
+      name: 'Marketing (Coupon Codes)',
+      path: "",
+    },
+    {
+      name: 'Marketing (Cart-Level Discounts)',
+      path: "",
+    },
+    {
+      name: 'Marketing (Email Marketing)',
+      path: "",
+    },
+    {
+      name: 'Marketing (Gift Certificates)',
+      path: "",
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
